b324596734c4bdb060b6a2657589ac4636e015aa
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-04-02 12:20:10 +0200

    feat: Page de maintenance

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>VP Lab's - Professeur particulier en mathématiques, physique et chimie</title>
+    <link rel="icon" href="public/assets/images/favicon.ico" />
+    <link rel="stylesheet" href="public/assets/css/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="maintenance-container">
+        <div class="maintenance-content">
+            <img src="public/assets/images/maintenance.svg" alt="">
+            <h1>Le site est actuellement en construction</h1>
+            <p>Tout n'est pas encore entièrement terminé, mais tu peux déjà me retrouver sur <a href="https://app.vplabs.fr">app.vplabs.fr</a>.</p>
+        </div>
+    </div>
+</body>
+</html>
